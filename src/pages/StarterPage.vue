6dfa1852fd14a0668644c171e810ffe38b9a808a
wumoxi <wu.shaohua@foxmail.com>
2020-03-01 12:44:14 +0800

    add index content

--- src/pages/StarterPage.vue
+++ src/pages/StarterPage.vue
@@ -1,132 +1,207 @@
 <template>
     <div class="starter-page">
-        <div class="section text-center">
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
-            <h3>Create your next awesome website(创建你的下一个伟大的网站)</h3>
+        <div class="section">
+            <div class="container">
+                <div class="col-md-8 ml-auto col-xl-8 mr-auto float-left left-content">
+                    <div class="row left-row">
+                        <div class="jumbotron jumbotron-border" style="position: relative; width: 100%;">
+                            <div class="badge badge-primary badge-custom">Golang</div>
+                            <a href="http://www.baidu.com">
+                                <h3 class="reset-h3">Golang结构体使用</h3>
+                            </a>
+                            <span>
+                                 <span class="badge badge-success">Golang</span>
+                            </span>
+                            <div class="lead markdown">
+                                Laravel中使用Event事件可以将代码进行解耦、使用项目变的更加易于维护和扩展。可以通过创建一个事件和一个或多个监听者对一个事件进行监听，当事件触发的时候，所对应的监听者就会执行。这比我们以往写代码处理一个响应时要方便的太多了，比如以往注册一个用户时你可能也许会将注册成功发送邮件通知的代码逻辑放入到一个方法中进行处理，那样的话代码显得非常的臃肿，也不利于后期维护和扩展，例如你们产品的注册成功邮件通知模板文件有相应的调整，那你也得去调整注册方法吧，那在laravel中使用Event事件加Notification通知会让这件事件变量非常优雅和便于后期维护和扩展。
+                            </div>
+                            <hr>
+                            <div class="lead">
+                                <span class="pull-left">
+                                    <span class="oi oi-timer">
+                                        <b>2000-20-22</b>
+                                    </span>
+                                </span>
+                                <span class="pull-right">
+                                    <span class="oi oi-comment-square"> <b>0</b> </span>
+                                    <span class="oi oi-eye"> <b>0</b> </span>
+                                    <span class="oi oi-thumb-up"> <b>0</b> </span>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row left-row">
+                        <div class="jumbotron jumbotron-border" style="position: relative; width: 100%;">
+                            <div class="badge badge-primary badge-custom">Golang</div>
+                            <a href="http://www.baidu.com">
+                                <h3 class="reset-h3">Golang结构体使用</h3>
+                            </a>
+                            <span>
+                                 <span class="badge badge-success">Golang</span>
+                            </span>
+                            <div class="lead markdown">
+                                Laravel中使用Event事件可以将代码进行解耦、使用项目变的更加易于维护和扩展。可以通过创建一个事件和一个或多个监听者对一个事件进行监听，当事件触发的时候，所对应的监听者就会执行。这比我们以往写代码处理一个响应时要方便的太多了，比如以往注册一个用户时你可能也许会将注册成功发送邮件通知的代码逻辑放入到一个方法中进行处理，那样的话代码显得非常的臃肿，也不利于后期维护和扩展，例如你们产品的注册成功邮件通知模板文件有相应的调整，那你也得去调整注册方法吧，那在laravel中使用Event事件加Notification通知会让这件事件变量非常优雅和便于后期维护和扩展。
+                            </div>
+                            <hr>
+                            <div class="lead">
+                                <span class="pull-left">
+                                    <span class="oi oi-timer">
+                                        <b>2000-20-22</b>
+                                    </span>
+                                </span>
+                                <span class="pull-right">
+                                    <span class="oi oi-comment-square"> <b>0</b> </span>
+                                    <span class="oi oi-eye"> <b>0</b> </span>
+                                    <span class="oi oi-thumb-up"> <b>0</b> </span>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row left-row">
+                        <div class="jumbotron jumbotron-border" style="position: relative; width: 100%;">
+                            <div class="badge badge-primary badge-custom">Golang</div>
+                            <a href="http://www.baidu.com">
+                                <h3 class="reset-h3">Golang结构体使用</h3>
+                            </a>
+                            <span>
+                                 <span class="badge badge-success">Golang</span>
+                            </span>
+                            <div class="lead markdown">
+                                Laravel中使用Event事件可以将代码进行解耦、使用项目变的更加易于维护和扩展。可以通过创建一个事件和一个或多个监听者对一个事件进行监听，当事件触发的时候，所对应的监听者就会执行。这比我们以往写代码处理一个响应时要方便的太多了，比如以往注册一个用户时你可能也许会将注册成功发送邮件通知的代码逻辑放入到一个方法中进行处理，那样的话代码显得非常的臃肿，也不利于后期维护和扩展，例如你们产品的注册成功邮件通知模板文件有相应的调整，那你也得去调整注册方法吧，那在laravel中使用Event事件加Notification通知会让这件事件变量非常优雅和便于后期维护和扩展。
+                            </div>
+                            <hr>
+                            <div class="lead">
+                                <span class="pull-left">
+                                    <span class="oi oi-timer">
+                                        <b>2000-20-22</b>
+                                    </span>
+                                </span>
+                                <span class="pull-right">
+                                    <span class="oi oi-comment-square"> <b>0</b> </span>
+                                    <span class="oi oi-eye"> <b>0</b> </span>
+                                    <span class="oi oi-thumb-up"> <b>0</b> </span>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row left-row">
+                        <div class="jumbotron jumbotron-border" style="position: relative; width: 100%;">
+                            <div class="badge badge-primary badge-custom">Golang</div>
+                            <a href="http://www.baidu.com">
+                                <h3 class="reset-h3">Golang结构体使用</h3>
+                            </a>
+                            <span>
+                                 <span class="badge badge-success">Golang</span>
+                            </span>
+                            <div class="lead markdown">
+                                Laravel中使用Event事件可以将代码进行解耦、使用项目变的更加易于维护和扩展。可以通过创建一个事件和一个或多个监听者对一个事件进行监听，当事件触发的时候，所对应的监听者就会执行。这比我们以往写代码处理一个响应时要方便的太多了，比如以往注册一个用户时你可能也许会将注册成功发送邮件通知的代码逻辑放入到一个方法中进行处理，那样的话代码显得非常的臃肿，也不利于后期维护和扩展，例如你们产品的注册成功邮件通知模板文件有相应的调整，那你也得去调整注册方法吧，那在laravel中使用Event事件加Notification通知会让这件事件变量非常优雅和便于后期维护和扩展。
+                            </div>
+                            <hr>
+                            <div class="lead">
+                                <span class="pull-left">
+                                    <span class="oi oi-timer">
+                                        <b>2000-20-22</b>
+                                    </span>
+                                </span>
+                                <span class="pull-right">
+                                    <span class="oi oi-comment-square"> <b>0</b> </span>
+                                    <span class="oi oi-eye"> <b>0</b> </span>
+                                    <span class="oi oi-thumb-up"> <b>0</b> </span>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row left-row">
+                        <div class="jumbotron jumbotron-border" style="position: relative; width: 100%;">
+                            <div class="badge badge-primary badge-custom">Golang</div>
+                            <a href="http://www.baidu.com">
+                                <h3 class="reset-h3">Golang结构体使用</h3>
+                            </a>
+                            <span>
+                                 <span class="badge badge-success">Golang</span>
+                            </span>
+                            <div class="lead markdown">
+                                Laravel中使用Event事件可以将代码进行解耦、使用项目变的更加易于维护和扩展。可以通过创建一个事件和一个或多个监听者对一个事件进行监听，当事件触发的时候，所对应的监听者就会执行。这比我们以往写代码处理一个响应时要方便的太多了，比如以往注册一个用户时你可能也许会将注册成功发送邮件通知的代码逻辑放入到一个方法中进行处理，那样的话代码显得非常的臃肿，也不利于后期维护和扩展，例如你们产品的注册成功邮件通知模板文件有相应的调整，那你也得去调整注册方法吧，那在laravel中使用Event事件加Notification通知会让这件事件变量非常优雅和便于后期维护和扩展。
+                            </div>
+                            <hr>
+                            <div class="lead">
+                                <span class="pull-left">
+                                    <span class="oi oi-timer">
+                                        <b>2000-20-22</b>
+                                    </span>
+                                </span>
+                                <span class="pull-right">
+                                    <span class="oi oi-comment-square"> <b>0</b> </span>
+                                    <span class="oi oi-eye"> <b>0</b> </span>
+                                    <span class="oi oi-thumb-up"> <b>0</b> </span>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 ml-auto col-xl-4 mr-auto float-right right-content">
+                    <div class="row">
+                        <el-card class="box-card" shadow="never">
+                            <div slot="header" class="clearfix">
+                                <span>卡片名称</span>
+                            </div>
+                            <el-table class="box-card-table" :data="tableData">
+                                <el-table-column prop="address" ></el-table-column>
+                            </el-table>
+                        </el-card>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 <script>
     export default {
-        name: 'starter'
+        name: 'starter',
+        data() {
+            return {
+                tableData: [{
+                    address: '上海市普陀区金沙江路 1518 弄'
+                }, {
+                    address: '上海市普陀区金沙江路 1517 弄'
+                }, {
+                    address: '上海市普陀区金沙江路 1519 弄'
+                }, {
+                    address: '上海市普陀区金沙江路 1516 弄'
+                }]
+            }
+        }
     };
 </script>
 <style>
     .starter-page {
         min-height: calc(100vh - 95px);
     }
+    .text {
+        font-size: 14px;
+    }
+
+    .item {
+        margin-bottom: 18px;
+    }
+
+    .clearfix:before,
+    .clearfix:after {
+        display: table;
+        content: "";
+    }
+    .clearfix:after {
+        clear: both
+    }
+
+    .box-card {
+        width: 480px;
+        background: rgba(255, 255, 255, 0.5);
+        box-shadow: rgba(255, 255, 255, 0.3) 0 1px 0, rgba(0, 0, 0, 0.3) 0 -1px 0;
+        box-shadow: 0 0 3px rgba(55, 55, 55, 0.7);
+    }
+    .box-card-table {
+        background: rgba(255, 255, 255, 0.0);
+    }
+
 </style>
