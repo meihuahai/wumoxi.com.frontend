a62ca8c3d2aa1aee6f1fa69b6352516f146be410
wumoxi <wu.shaohua@foxmail.com>
2020-04-06 10:59:22 +0800

    modified info page view

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,7 @@ module.exports = {
         // Enable CSS source maps.
         sourceMap: process.env.NODE_ENV !== 'production'
     },
-    publicPath: './',
+    publicPath: '/',
     /* 输出文件目录：在npm run build时，生成文件的目录名称 */
     outputDir: 'dist',
     /* 放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录 */
